d9b1e958c461a4fdd3b16c639b32991366ecbe4b
Caspar Qian <fakedestinyck@gmail.com>
2020-05-02 14:06:49 +0800

    deploy

---
+++ js/precache-manifest.04b7c426cf5ea4f8feb54029dd1a79d6.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed92847dcf3d55ea75e1",
+    "url": "/css/app.d1757a7c.css"
+  },
+  {
+    "revision": "6a0598d56fbca7846bbe",
+    "url": "/css/chunk-47878536.b21ac4e5.css"
+  },
+  {
+    "revision": "6f6ad22961022cec5494",
+    "url": "/css/chunk-5306e6cb.10edf29e.css"
+  },
+  {
+    "revision": "c2d2ebc1c32332db4f05",
+    "url": "/css/chunk-vendors.c92635ab.css"
+  },
+  {
+    "revision": "5fad700adc948cb51404d55833347f51",
+    "url": "/fonts/feather.5fad700a.eot"
+  },
+  {
+    "revision": "66cbb621b431bf32041a5c478e5539c0",
+    "url": "/fonts/feather.66cbb621.woff"
+  },
+  {
+    "revision": "a940fe89dbfe9d1d89fc1aa0488fe032",
+    "url": "/fonts/feather.a940fe89.ttf"
+  },
+  {
+    "revision": "023ba0824a99109c4a63622228dd6354",
+    "url": "/img/feather.023ba082.svg"
+  },
+  {
+    "revision": "1f162d54893767cea8acb91c3bb150b4",
+    "url": "/img/logo.1f162d54.png"
+  },
+  {
+    "revision": "c9b3fe1ad30f4c51672c16aca744bd6e",
+    "url": "/img/logo_horizontal.c9b3fe1a.png"
+  },
+  {
+    "revision": "02a29277165eea8a223b414d5a1185e7",
+    "url": "/img/logo_only.02a29277.png"
+  },
+  {
+    "revision": "67ea995cad38f54db9195b499ba64b56",
+    "url": "/img/logo_transparent/logo.png"
+  },
+  {
+    "revision": "c9b3fe1ad30f4c51672c16aca744bd6e",
+    "url": "/img/logo_transparent/logo_horizontal.png"
+  },
+  {
+    "revision": "1f162d54893767cea8acb91c3bb150b4",
+    "url": "/img/logo_transparent/logo_no_description.png"
+  },
+  {
+    "revision": "02a29277165eea8a223b414d5a1185e7",
+    "url": "/img/logo_transparent/logo_only.png"
+  },
+  {
+    "revision": "54f9f9bb0305dd5b94a06b5199e9d531",
+    "url": "/img/logo_white/logo.jpg"
+  },
+  {
+    "revision": "0655de402179788aa3d0b3953d29f5c5",
+    "url": "/img/logo_white/logo.png"
+  },
+  {
+    "revision": "50acac7d4db45f92f36e55171b3cfa86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dccd97a4c2ad2afa6d8",
+    "url": "/js/about.6f6ca75f.js"
+  },
+  {
+    "revision": "ed92847dcf3d55ea75e1",
+    "url": "/js/app.33ef59ff.js"
+  },
+  {
+    "revision": "6a0598d56fbca7846bbe",
+    "url": "/js/chunk-47878536.04611e02.js"
+  },
+  {
+    "revision": "6f6ad22961022cec5494",
+    "url": "/js/chunk-5306e6cb.0d3dbc09.js"
+  },
+  {
+    "revision": "c2d2ebc1c32332db4f05",
+    "url": "/js/chunk-vendors.bc308f84.js"
+  },
+  {
+    "revision": "61f6bb01f8fde891a92bcff7f88c7ede",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
